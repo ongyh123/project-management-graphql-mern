915b89e9c415bdf7be535b3e8af7d3b3a2358b3f
Ong Yue Hian <ong.0127@gmail.com>
2023-06-06 16:29:38 +0800

    add clients to schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,6 +5,7 @@ const {
   GraphQLID,
   GraphQLString,
   GraphQLSchema,
+  GraphQLList,
 } = require("graphql");
 
 const ClientType = new GraphQLObjectType({
@@ -20,6 +21,12 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return clients;
+      },
+    },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
