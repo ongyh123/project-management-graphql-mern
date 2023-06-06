19a42957ad5161b63dffc3d51c92bda60b30414a
Ong Yue Hian <ong.0127@gmail.com>
2023-06-07 00:01:15 +0800

    Add Clients component to App, using apolloClient

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import Header from "./components/Header"
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import Clients from "./components/Clients";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="container">
-        <h1>Hello World!</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <Router>
+          <Header />
+          <div className="container">
+            <Clients />
+          </div>
+        </Router>
+      </ApolloProvider>
     </>
   );
 }
