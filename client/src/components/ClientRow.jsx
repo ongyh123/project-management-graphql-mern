3bef52506d262259f622982a7094324f190b3949
Ong Yue Hian <ong.0127@gmail.com>
2023-06-07 00:22:19 +0800

    add deleteClient function to ClientRow.jsx

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,6 +1,12 @@
 import { FaTrash } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+import { DELETE_CLIENT } from "../mutations/clientMutations";
 
 export default function ClientRow({ client }) {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+  });
+
   return (
     <tr>
       <td>{client.name}</td>
@@ -9,7 +15,7 @@ export default function ClientRow({ client }) {
       <td>
         <button
           className="btn btn-danger btn-sm"
-          onClick={()=>{}}
+          onClick={deleteClient}
         >
           <FaTrash />
         </button>
