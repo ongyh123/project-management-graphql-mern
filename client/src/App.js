ae9d7bb92c8e86dce0afee3acb0eca5163ecf79e
Ong Yue Hian <ong.0127@gmail.com>
2023-06-07 01:01:24 +0800

    Add <Projects /> to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Clients from "./components/Clients";
 import AddClientModal from "./components/AddClientModal";
+import Projects from "./components/Projects";
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -36,6 +37,7 @@ function App() {
           <Header />
           <div className="container">
             <AddClientModal />
+            <Projects />
             <Clients />
           </div>
         </Router>
